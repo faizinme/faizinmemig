5e518349ae8d90422d2ab4d6a19b1d24a2d1dc17
Nur Faizin <faizin.apps@gmail.com>
2024-08-05 02:20:48 +0700

    SITE_DESCRIPTION

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Nur Faizin | From Creative Enthusiast ';
+export const SITE_DESCRIPTION = 'Specializing in web development, open source, and web hosting technical support.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
